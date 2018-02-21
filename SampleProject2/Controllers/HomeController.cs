76535ce3ce41405791dcddff9ecf2c85e4f34700
Kingsley Akpan <kingsleyakpan18@gmail.com>
2018-02-20 23:52:21 -0800

    login and logout completed

---
+++ SampleProject2/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SampleProject2.Models;
+
+namespace SampleProject2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Models.User user)
+        {
+            using (SampleProjectEntities db = new SampleProjectEntities())
+            {
+                var userDetails = db.Users.Where(x => x.UserName == user.UserName && x.UserPassword == user.UserPassword).FirstOrDefault();
+
+                if (userDetails == null)
+                {
+
+                    user.LoginErrorMsg = "you have enter an incorrect password or user name";
+
+                    return View("index", user);
+                }
+                else
+                {
+                    Session["UserID"] = userDetails.UserID;
+
+                    return RedirectToAction("index", "DashBoard");
+                }
+
+            }
+
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+
+            return RedirectToAction("index");
+        }
+    }
+}
